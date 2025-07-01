d65c8b24ea48c9e2d1815eb045f24fc8760fa64e
yamamoto.taizo <taizod1024@gmail.com>
2025-07-01 20:17:52 +0900

    feat: ファイルをcmdとpowershellで開く

--- src/EverythingExtension.ts
+++ src/EverythingExtension.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
-const fs = require("fs");
+import * as path from "path";
 
 /** everything-search-extesnion class */
 class EverythingExtension {
@@ -96,11 +96,11 @@ class EverythingExtension {
         const selectedItem = quickPick.selectedItems[0];
         if (selectedItem && selectedItem.label.match(/[\\/]/)) {
           quickPick.hide();
-          const path = selectedItem.label;
+          const pathToAny = selectedItem.label;
           const type = selectedItem.description;
           const config = vscode.workspace.getConfiguration(this.appCfgKey);
           if (config.debug) {
-            this.channel.appendLine(`debug: selected='${path}'`);
+            this.channel.appendLine(`debug: selected='${pathToAny}'`);
           }
           const OPEN_WITH_VSCODE = "open with vscode";
           const OPEN_WITH_EXPLORER = "open with explorer";
@@ -109,23 +109,31 @@ class EverythingExtension {
           const OPEN_WITH_CMD = "open with cmd";
           const OPEN_WITH_POWERSHELL = "open with powershell";
           if (type !== "\\") {
-            vscode.window.showQuickPick([OPEN_WITH_VSCODE, OPEN_WITH_EXPLORER, OPEN_WITH_DEFAULT_APPLICATION, COPY_PATH_TO_CLIPBOARD], { placeHolder: "Choose how to open the selected file" }).then(async selection => {
+            vscode.window.showQuickPick([OPEN_WITH_VSCODE, OPEN_WITH_EXPLORER, OPEN_WITH_CMD, OPEN_WITH_POWERSHELL, OPEN_WITH_DEFAULT_APPLICATION, COPY_PATH_TO_CLIPBOARD], { placeHolder: "Choose how to open the selected file" }).then(async selection => {
               if (!selection) {
                 return;
               }
               switch (selection) {
                 case OPEN_WITH_VSCODE:
-                  vscode.commands.executeCommand("vscode.open", vscode.Uri.file(path));
+                  vscode.commands.executeCommand("vscode.open", vscode.Uri.file(pathToAny));
                   break;
                 case OPEN_WITH_EXPLORER:
-                  require("child_process").exec(`explorer /select,"${path}"`);
+                  require("child_process").exec(`explorer /select,"${pathToAny}"`);
+                  break;
+                case OPEN_WITH_CMD:
+                  const parentPath1 = path.dirname(pathToAny);
+                  require("child_process").exec(`start cmd /K "cd /d ${parentPath1}"`);
+                  break;
+                case OPEN_WITH_POWERSHELL:
+                  const parentPath2 = path.dirname(pathToAny);
+                  require("child_process").exec(`start powershell -NoExit -Command "cd '${parentPath2}'"`);
                   break;
                 case OPEN_WITH_DEFAULT_APPLICATION:
-                  require("child_process").exec(`explorer "${path}"`);
+                  require("child_process").exec(`explorer "${pathToAny}"`);
                   break;
                 case COPY_PATH_TO_CLIPBOARD:
-                  await vscode.env.clipboard.writeText(path);
-                  vscode.window.showInformationMessage(`Path copied to clipboard: ${path}`);
+                  await vscode.env.clipboard.writeText(pathToAny);
+                  vscode.window.showInformationMessage(`Path copied to clipboard: ${pathToAny}`);
                   break;
               }
             });
@@ -136,20 +144,20 @@ class EverythingExtension {
               }
               switch (selection) {
                 case OPEN_WITH_VSCODE:
-                  vscode.commands.executeCommand("vscode.openFolder", vscode.Uri.file(path), { forceNewWindow: true });
+                  vscode.commands.executeCommand("vscode.openFolder", vscode.Uri.file(pathToAny), { forceNewWindow: true });
                   break;
                 case OPEN_WITH_EXPLORER:
-                  require("child_process").exec(`explorer "${path}"`);
+                  require("child_process").exec(`explorer "${pathToAny}"`);
                   break;
                 case OPEN_WITH_CMD:
-                  require("child_process").exec(`start cmd /K "cd /d ${path}"`);
+                  require("child_process").exec(`start cmd /K "cd /d ${pathToAny}"`);
                   break;
                 case OPEN_WITH_POWERSHELL:
-                  require("child_process").exec(`start powershell -NoExit -Command "cd '${path}'"`);
+                  require("child_process").exec(`start powershell -NoExit -Command "cd '${pathToAny}'"`);
                   break;
                 case COPY_PATH_TO_CLIPBOARD:
-                  await vscode.env.clipboard.writeText(path);
-                  vscode.window.showInformationMessage(`Path copied to clipboard: ${path}`);
+                  await vscode.env.clipboard.writeText(pathToAny);
+                  vscode.window.showInformationMessage(`Path copied to clipboard: ${pathToAny}`);
                   break;
               }
             });
